307287196929d666578265b8a499fc79d9377192
Samuel CALEGARI <samuel.calegari@gmail.com>
2022-06-03 11:16:12 +0200

    add pausetime et maxtime config

--- mod_form.php
+++ mod_form.php
@@ -90,14 +90,14 @@ class mod_guacamole_mod_form extends moodleform_mod {
         if($data['timeopen'] > $data['timeclose'])
             $errors['timeopen'] = get_string('err1', 'mod_guacamole');
 
-        if($data['timeclose'] - $data['timeopen'] > (4 * 60 * 60))
+        if($data['timeclose'] - $data['timeopen'] > (get_string('configmaxtime', 'guacamole') * 60))
             $errors['timeclose'] = get_string('err2', 'mod_guacamole');
 
         if(!isFree($data['timeopen'], $data['timeclose'], $data["instance"]))
-            $errors['timeopen'] = get_string('err3', 'mod_guacamole');
+            $errors['timeopen'] = get_string('err3', 'mod_guacamole').' : ' . get_string('configmaxtime', 'guacamole') .  ' mins';
 
-        if(!isPauseTimeRespected($data['timeopen'], $data['timeclose'], $data["instance"], 15*60))
-            $errors['timeopen'] = get_string('err4', 'mod_guacamole');
+        if(!isPauseTimeRespected($data['timeopen'], $data['timeclose'], $data["instance"], get_string('configpausetime', 'guacamole')*60))
+            $errors['timeopen'] = get_string('err4', 'mod_guacamole').' : ' . get_string('configpausetime', 'guacamole') .  ' mins';
 
         return $errors;
     }
