6e6248febf5ca1725d03c5590c0a74c27bfb3dfc
Samuel CALEGARI <samuel.calegari@gmail.com>
2022-06-03 11:14:14 +0200

    add pausetime et maxtime config

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Current version ##
 
-1.1
+1.2
 
 ## Features ##
 - Add virtual classroom, provided by Apache Guacamole in Moodle
--- settings.php
+++ settings.php
@@ -15,4 +15,10 @@ if ($ADMIN->fulltree) {
     //--- general settings -----------------------------------------------------------------------------------
     $settings->add(new admin_setting_configtext('guacamole/url', get_string('url', 'guacamole'),
         get_string('configurl', 'guacamole'), '', PARAM_URL));
+
+    $settings->add(new admin_setting_configtext('guacamole/maxtime', get_string('maxtime', 'guacamole'),
+        get_string('configmaxtime', 'guacamole'), '240', PARAM_INT));
+
+    $settings->add(new admin_setting_configtext('guacamole/pausetime', get_string('pausetime', 'guacamole'),
+        get_string('configpausetime', 'guacamole'), '15', PARAM_INT));
 }
